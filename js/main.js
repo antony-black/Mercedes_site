bd08b58e5152535740aa4297b1456c0d0e36c2b1
antony-black <98949182+antony-black@users.noreply.github.com>
2022-08-17 18:51:42 +0200

    Add files via upload
    Applied html, scss, grid, flex box, jquery, BEM, adaptive layout

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+        $('.slider-block').slick({
+          slidesToShow: 4,
+          variableWidth: true,
+          prevArrow:'<button type="button" class="arrow arrow-left"><img src="img/slider/arrow_left.svg" alt="left"></button>',
+          nextArrow:'<button type="button" class="arrow arrow-right"><img src="img/slider/arrow_right.svg" alt="right"></button>'
+
+        })
+
+
+
+
+});
